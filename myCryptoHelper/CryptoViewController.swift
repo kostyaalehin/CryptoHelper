4af46d2fe3dc48e9dd4b40f8ce89a6ce23ce90b5
kostyaalehin <koss595959@gmail.com>
2022-07-03 09:37:51 +0500

    connect crypto with viewController

--- myCryptoHelper/CryptoViewController.swift
+++ myCryptoHelper/CryptoViewController.swift
@@ -11,6 +11,8 @@ class CryptoViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
 
+    private var viewModels = [CryptoTableViewCellViewModel]()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,11 +20,20 @@ class CryptoViewController: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
 
-        NomicsAPICaller.shared.getAllCryptoData { result in
+        NomicsAPICaller.shared.getAllCryptoData { [weak self] result in
             switch result {
             case .success(let models):
                 print(models.count)
                 print(models[0].name! + " " + models[0].price!)
+                self?.viewModels = models.compactMap({
+                    CryptoTableViewCellViewModel(
+                        name: $0.name ?? "", symbol: $0.symbol ?? "", price: $0.price ?? "")
+                })
+
+                DispatchQueue.main.async {
+                    self?.tableView.reloadData()
+                }
+
             case .failure(_):
                 print("no good")
             }
@@ -33,13 +44,14 @@ class CryptoViewController: UIViewController {
 extension CryptoViewController: UITableViewDelegate, UITableViewDataSource {
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return viewModels.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: CryptoTableViewCell.identifier, for: indexPath) as? CryptoTableViewCell else {
             fatalError()
         }
+        cell.configure(with: viewModels[indexPath.row])
         return cell
     }
 }
